6672c7479ec7f08317378d5ed678d5e53239f966
Ronny Ancorini <Ronnygx@gmail.com>
2016-04-23 19:37:29 +0100

    green: start_with_1000_points_as_health

---
+++ Tests/CharacterShould.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CharacterShould
+    {
+        /*
+			- start with 1000 point as health
+            - start at level 1
+            - characters have two states live or dead
+            - deal damage
+            - heal characters
+		 */
+
+        [Test]
+        public void start_with_1000_points_as_health()
+        {
+            new Character().Life.Should().Be(1000);
+        }
+    }
+
+    public class Character
+    {
+        public int Life { get; } = 1000;
+    }
+}
