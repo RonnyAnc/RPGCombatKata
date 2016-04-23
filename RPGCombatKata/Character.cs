b674a0a8c507466f0b82e7fa1c8d7e95f0dce205
Ronny Ancorini <Ronnygx@gmail.com>
2016-04-23 19:41:09 +0100

    green: start_with_1000_points_as_health

--- RPGCombatKata/Character.cs
+++ RPGCombatKata/Character.cs
@@ -3,5 +3,6 @@
     public class Character
     {
         public int Life { get; } = 1000;
+        public int Level { get; } = 1;
     }
 }
--- Tests/CharacterShould.cs
+++ Tests/CharacterShould.cs
@@ -8,7 +8,6 @@ namespace Tests
     public class CharacterShould
     {
         /*
-			- start with 1000 point as health
             - start at level 1
             - characters have two states live or dead
             - deal damage
@@ -19,6 +18,12 @@ namespace Tests
         public void start_with_1000_points_as_health()
         {
             ACharacter().Life.Should().Be(1000);
+        }
+
+        [Test]
+        public void start_at_level_1()
+        {
+            ACharacter().Level.Should().Be(1);
         }
 
         private static Character ACharacter()
