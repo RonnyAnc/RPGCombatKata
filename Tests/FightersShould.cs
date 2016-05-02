8516bab85ee9276a27a497852a26c68b3a0303e7
Ronny Ancorini <Ronnygx@gmail.com>
2016-05-02 01:45:02 +0100

    Refactor: use a class to test ranges of different types of characters

---
+++ Tests/FightersShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using RPGCombatKata;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FightersShould
+    {
+        [Test]
+        public void have_an_attack_range_of_2_metters_when_is_a_melee_fighter()
+        {
+            new MeleeFigther().AttackRange.Should().Be(2);
+        }
+
+        [Test]
+        public void have_an_attack_range_of_20_metters_when_is_a_ranged_fighter()
+        {
+            new RangedFigther().AttackRange.Should().Be(20);
+        }
+    }
+}
